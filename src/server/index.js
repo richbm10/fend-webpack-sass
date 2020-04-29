fa899c37d9bd91e52a2a58bafe29ae2f8e63b1ca
richbm10 <ricardobonilla99@hotmail.com>
2020-04-29 10:45:00 -0600

    Client-JS-webpack-library

--- src/server/index.js
+++ src/server/index.js
@@ -4,19 +4,21 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+const port = 8081;
+
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}!`)
 })
 
-app.get('/test', function (req, res) {
+app.get('/test', function(req, res) {
     res.send(mockAPIResponse)
 })
